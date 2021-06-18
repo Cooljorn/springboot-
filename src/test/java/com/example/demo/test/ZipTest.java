72b3fe47e401a30d7802f809dfc618638ccf92cb
chengshuaishuai <chengshuaishuai@cangoonline.com>
2021-06-18 17:39:13 +0800

    三种压缩方式比较

--- src/test/java/com/example/demo/test/ZipTest.java
+++ src/test/java/com/example/demo/test/ZipTest.java
@@ -1,11 +1,9 @@
 package com.example.demo.test;
 
 import com.example.demo.util.SoutUtil;
+import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.WritableByteChannel;
@@ -14,45 +12,64 @@ import java.time.temporal.ChronoUnit;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-public class ZipTest {
-    public static String ZIP_FILE = "";
-    public static String JPG_FILE = "";
-    public static String FILE_NAME = "";
-    public static String SUFFIX_FILE = "";
-    public static long FILE_SIZE = 10;
 
-    public static void zipFileNoBuffer() {
+public class ZipTest {
+    public static String ZIP_FILE = "D:\\out\\zip.zip";
+    public static String FILE_PATH = "D:\\out\\JAVA核心知识点整理.pdf";
+    public static String FILE_NAME = "111.pdf";
+    public static String SUFFIX_FILE = "";
+    public static long FILE_SIZE = 1;
+
+    @Test
+    public  void zipFileNoBuffer() {
 
         File zipFile = new File(ZIP_FILE);
         try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile))) {
             //开始时间
             Instant beginTime = Instant.now();
-
-            for (int i = 0; i < 10; i++) {
-                try (InputStream input = new FileInputStream(JPG_FILE)) {
-                    zipOut.putNextEntry(new ZipEntry(FILE_NAME + i));
-                    int temp = 0;
-                    while ((temp = input.read()) != -1) {
-                        zipOut.write(temp);
-                    }
+            try (InputStream input = new FileInputStream(FILE_PATH)) {
+                zipOut.putNextEntry(new ZipEntry(FILE_NAME));
+                int temp = 0;
+                while ((temp = input.read()) != -1) {
+                    zipOut.write(temp);
                 }
             }
+
             SoutUtil.betweenTimeMs(beginTime);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void zipFileChannel() {
+    @Test
+    public void zipFileBuffer() {
+        File zipFile = new File(ZIP_FILE);
+        try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile));
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(zipOut)) {
+            //开始时间
+            Instant beginTime = Instant.now();
+            try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(FILE_PATH))) {
+                zipOut.putNextEntry(new ZipEntry(FILE_NAME));
+                int temp = 0;
+                while ((temp = bufferedInputStream.read()) != -1) {
+                    bufferedOutputStream.write(temp);
+                }
+            }
+
+            SoutUtil.betweenTimeMs(beginTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void zipFileChannel() {
         //开始时间
         Instant beginTime = Instant.now();
         File zipFile = new File(ZIP_FILE);
         try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile));
              WritableByteChannel writableByteChannel = Channels.newChannel(zipOut)) {
-            for (int i = 0; i < 10; i++) {
-                try (FileChannel fileChannel = new FileInputStream(JPG_FILE).getChannel()) {
-                    zipOut.putNextEntry(new ZipEntry(i + SUFFIX_FILE));
-                    fileChannel.transferTo(0, FILE_SIZE, writableByteChannel);
-                }
+            try (FileChannel fileChannel = new FileInputStream(FILE_PATH).getChannel()) {
+                zipOut.putNextEntry(new ZipEntry( FILE_NAME));
+                fileChannel.transferTo(0, FILE_SIZE, writableByteChannel);
             }
             SoutUtil.betweenTimeMs(beginTime);
         } catch (Exception e) {
