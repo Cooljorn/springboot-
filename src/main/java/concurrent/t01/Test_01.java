126c52535bc8a87143692487c9c602f73858969e
chengshuaishuai <chengshuaishuai@cangoonline.com>
2022-02-25 16:41:27 +0800

    并发学习代码

---
+++ src/main/java/concurrent/t01/Test_01.java
@@ -0,0 +1,78 @@
+/**
+ * synchronized关键字
+ * 锁对象。synchronized(this)和synchronized方法都是锁当前对象。
+ */
+package concurrent.t01;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test_01 {
+	private Integer count = 0;
+	private Object o = new Object();
+	
+	public void testSync1(){
+		synchronized(o){
+			System.out.println(Thread.currentThread().getName() 
+					+ " count = " + count++);
+			try {
+				TimeUnit.SECONDS.sleep(3);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void testSync2(){
+		synchronized(this){
+			System.out.println(Thread.currentThread().getName() 
+					+ " count = " + count++);
+			try {
+				TimeUnit.SECONDS.sleep(3);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()+" done !!");
+			System.out.println(Thread.currentThread().getName()
+					+ " count = " + count++);
+		}
+	}
+	
+	public synchronized void testSync3(){
+		System.out.println(Thread.currentThread().getName() 
+				+ " count = " + count++);
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	public void testSync4(){
+		synchronized(count) {
+			System.out.println(Thread.currentThread().getName()
+					+ " count = " + count++);
+			try {
+				TimeUnit.SECONDS.sleep(3);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()+" done !!");
+		}
+	}
+	
+	public static void main(String[] args) {
+		final Test_01 t = new Test_01();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				t.testSync4();
+			}
+		}).start();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				t.testSync4();
+			}
+		}).start();
+	}
+	
+}
